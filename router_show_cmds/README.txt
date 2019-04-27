73e261d5da509f3d0ca16bcb5d620990314294f9
Deron Ringen <deron.ringen@ibm.com>
2019-04-27 10:00:57 -0400

    Final Commit for show_cmds.py on 4/27/19

---
+++ router_show_cmds/README.txt
@@ -0,0 +1,20 @@
+##########################################
+# Requires package: python3-netmiko
+##########################################
+Run it with the following command line:
+python3 name_of_program.py -c device_name_or_ip.csv
+
+##########################################
+# You will need a CSV file with the following
+# structure (must include "device_ip"):
+##########################################
+device_ip
+10.10.1.1
+10.10.1.2
+10.10.1.3
+
+##########################################
+# Username/Password can be hardcoded in
+# program or user "prompted".  This setting
+# is done via comments in the program.
+##########################################
--- router_show_cmds/show_cmds_final.py
+++ router_show_cmds/show_cmds_final.py
@@ -12,14 +12,27 @@
 #10.10.1.2
 #10.10.1.3
 #
-###########################################
-# Remember to set the section the username/password section below
-###########################################
-
+###############################################################
+# Remember to set the username/password section below
+###############################################################
 import getpass
+############## USERNAME/PASSWORD SECTION ######################
+##### Uncommmit next two line for user input of username/password ###
+#ssh_username = input("SSH username: ")
+#ssh_password = getpass.getpass('SSH Password: ')
+##### Uncommit next two lines for hardcoded username/password ###
+ssh_username = "cisco"
+ssh_password = "cisco"
+###############################################################
+
+############## Create Output & Log Files ######################
+##### Change the name of the Output Log Files if needed #######
+fileout=open("show_cmds_out.txt","w+")
+filelog=open("show_cmds_log.txt","w+")
+################################################################
+
 import re
 import csv
-
 from argparse import ArgumentParser
 from netmiko import ConnectHandler
 from datetime import date
@@ -32,18 +45,6 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--csv', required=True, action='store', help='Location of CSV file')
     args = parser.parse_args()
 
-    ############## USERNAME/PASSWORD SECTION ######################
-    ##### Uncommmit next two line for user input of username/password ###
-    #ssh_username = input("SSH username: ")
-    #ssh_password = getpass.getpass('SSH Password: ')
-    ##### Uncommit next two lines for hardcoded username/password ###
-    ssh_username = "cisco"
-    ssh_password = "cisco"
-    ###############################################################
-
-    ### Create Output File ###
-    fileout=open("show_cmds_out.txt","w+")
-
 ### Begin Main loop ###
 print("Beginning Connections @ " + format(datetime.today()))
 with open(args.csv, "r") as file:
@@ -52,7 +53,10 @@ with open(args.csv, "r") as file:
             ### SSH to Device ###
             try:
                 ssh_session = ConnectHandler(device_type='cisco_ios', ip=device_row['device_ip'], username=ssh_username, password=ssh_password)
+
+                ### Succesful Connection ###
                 print("Connection Successful to " + device_row['device_ip'] + " @ " + format(datetime.today()))
+                filelog.write("Connection Successful to " + device_row['device_ip'] + " @ " + format(datetime.today()) + "\n\r")
                 fileout.write("===Network Device: " + device_row['device_ip'] + " @ "+ format(datetime.today()) + "\n\r")
 
                 ### Commands to Run on Device ###
@@ -62,8 +66,9 @@ with open(args.csv, "r") as file:
                 ssh_session.disconnect()
 
             except:
-                ### In  Case of a Failed SSH Connection ###
+                ### Failed Connection ###
                 print("Connection Failed to " + device_row['device_ip'] + " @ " + format(datetime.today()))
+                filelog.write("Connection Failed to " + device_row['device_ip'] + " @ " + format(datetime.today())  + "\n\r" )
                 fileout.write("***Connection Failure: " + device_row['device_ip'] + " @ "+ format(datetime.today()) + "\n\r")
 
         print("Finished")
